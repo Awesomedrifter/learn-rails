1d5e20f373b43d90598e0beeab3db0aa478dcd8d
awesomedrifter <awsomedrifter@gmail.com>
2016-02-20 21:16:43 +0000

    send mail

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "do-not-reply@blogoferic.com"
+  layout 'mailer'
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,12 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+  config.action_mailer.smtp_settings = {
+address: "smtp.mandrillapp.com",
+port: 587,
+user_name: Rails.application.secrets.email_provider_username,
+password: Rails.application.secrets.email_provider_api_key
+}
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
@@ -38,12 +44,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.smtp_settings = {
-address: "smtp.mandrillapp.com",
-port: 587,
-user_name: Rails.application.secrets.email_provider_username,
-password: Rails.application.secrets.email_provider_api_key
-}
+  
+
+# Send email in development mode?
+config.action_mailer.perform_deliveries = true
 # ActionMailer Config
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
